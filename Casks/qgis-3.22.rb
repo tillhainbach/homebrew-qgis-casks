962bd4cd65c5eb92e0b3669f74b1be128686b233
Till Hainbach <Till.Hainbach@posteo.de>
2021-11-05 18:52:00 +0100

    fix(casks): fix qgis-3.22 target

--- Casks/qgis-3.22.rb
+++ Casks/qgis-3.22.rb
@@ -17,7 +17,7 @@ cask "qgis-3.22" do
     end
   end
 
-  app "QGIS.app", target "QGIS 3.22.app"
+  app "QGIS.app", target: "QGIS 3.22.app"
 
   zap trash: [
     "~/Library/Application Support/QGIS",
